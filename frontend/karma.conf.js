7deee462066a2549102d14524c77090f409ef802
hettecmantras <het.tecmantras@gmail.com>
2026-07-17 12:10:13 +0530

    Add Angular workspace files and app shell

---
+++ frontend/karma.conf.js
@@ -0,0 +1,29 @@
+const { join } = require('path');
+
+module.exports = function (config) {
+  config.set({
+    basePath: '',
+    frameworks: ['jasmine', '@angular-devkit/build-angular'],
+    plugins: [
+      require('karma-jasmine'),
+      require('karma-chrome-launcher'),
+      require('karma-coverage'),
+      require('karma-jasmine-html-reporter'),
+      require('@angular-devkit/build-angular/plugins/karma')
+    ],
+    client: {
+      clearContext: false
+    },
+    coverageReporter: {
+      dir: join(__dirname, './coverage'),
+      reporters: [{ type: 'html' }, { type: 'text-summary' }]
+    },
+    reporters: ['progress', 'kjhtml'],
+    port: 9876,
+    colors: true,
+    logLevel: config.LOG_INFO,
+    autoWatch: true,
+    browsers: ['ChromeHeadless'],
+    restartOnFileChange: true
+  });
+};
